0b54530d680e181002338d229ce2344a9d087e3e
Balint <Balint@balint.com>
2018-06-29 18:41:43 +0200

    adding data model: Subscription

---
+++ src/main/java/org/marketing/newsletter/model/Subscription.java
@@ -0,0 +1,26 @@
+package org.marketing.newsletter.model;
+
+public class Subscription {
+	
+	private String fullName;
+	
+	private String emailAdress;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmailAdress() {
+		return emailAdress;
+	}
+
+	public void setEmailAdress(String emailAdress) {
+		this.emailAdress = emailAdress;
+	}
+	
+	
+}
