61653e8dc43581b6fd883f2511dc2dd49d3f7493
Balint <Balint@balint.com>
2018-07-03 21:56:47 +0200

    adding run method to NewsletterApplication

--- src/main/java/org/marketing/newsletter/NewsletterApplication.java
+++ src/main/java/org/marketing/newsletter/NewsletterApplication.java
@@ -1,12 +1,33 @@
 package org.marketing.newsletter;
 
+import org.marketing.newsletter.model.Subscription;
+import org.marketing.newsletter.service.SubscriptionService;
+import org.marketing.newsletter.service.exception.SubscriptionAlreadyExistsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewsletterApplication {
+public class NewsletterApplication implements CommandLineRunner {
 
+	@Autowired
+	private SubscriptionService service;
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsletterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Subscription subscription = new Subscription();
+		subscription.setFullName("Jhon Doe");
+		subscription.setEmailAdress("Jhon@email.com");
+		try {
+			service.register(subscription);
+		} catch (SubscriptionAlreadyExistsException duplicate) {
+			System.err.println("Sorry, there is already a registration with this e-mail adress");
+		}
+		
 	}
 }
