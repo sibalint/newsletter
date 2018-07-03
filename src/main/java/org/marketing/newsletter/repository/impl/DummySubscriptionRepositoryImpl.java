3345c0709329cdb0a2b9d70e3eaf1fe052bbb32e
Balint <Balint@balint.com>
2018-07-03 22:09:40 +0200

    adding Logger

--- src/main/java/org/marketing/newsletter/repository/impl/DummySubscriptionRepositoryImpl.java
+++ src/main/java/org/marketing/newsletter/repository/impl/DummySubscriptionRepositoryImpl.java
@@ -2,14 +2,18 @@ package org.marketing.newsletter.repository.impl;
 
 import org.marketing.newsletter.model.Subscription;
 import org.marketing.newsletter.repository.SubscriptionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class DummySubscriptionRepositoryImpl implements SubscriptionRepository {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Override
 	public void save(Subscription subscription) {
-		System.out.println("DummySubscriptionRepositoryImpl: 'saves' " + subscription);
+		log.info("'saves' {} now..." + subscription);
 
 	}
 
--- src/main/java/org/marketing/newsletter/service/impl/SumscriptionServiceImpl.java
+++ src/main/java/org/marketing/newsletter/service/impl/SumscriptionServiceImpl.java
@@ -4,22 +4,27 @@ import org.marketing.newsletter.model.Subscription;
 import org.marketing.newsletter.repository.SubscriptionRepository;
 import org.marketing.newsletter.service.SubscriptionService;
 import org.marketing.newsletter.service.exception.SubscriptionAlreadyExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SumscriptionServiceImpl implements SubscriptionService {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private SubscriptionRepository repository;
 	
 	@Override
 	public void register(Subscription subscription) throws SubscriptionAlreadyExistsException {
-		System.out.println("SubscriptionServiceImpl registers" + subscription);
+		log.info("Registring subscription: {}", subscription);
 		
 		try {
 		repository.save(subscription);
 		} catch (RuntimeException duplicate) { //FIXME:  add proper exception type
+			log.error("Error during persisting the subscription: {}", duplicate);
 			throw new SubscriptionAlreadyExistsException("Subscription already exists with email: " + subscription.getEmailAdress());
 		}
 	}
