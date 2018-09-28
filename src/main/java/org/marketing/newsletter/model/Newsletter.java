3daa1e199580f3df5b310767232a526f6c6c28ae
Balint Sipos <sipos.balint94@gmail.com>
2018-09-28 12:31:28 +0200

    Add: Model: newsletter

---
+++ src/main/java/org/marketing/newsletter/model/Newsletter.java
@@ -0,0 +1,39 @@
+package org.marketing.newsletter.model;
+
+public class Newsletter {
+
+    private String subject;
+    private String content;
+
+    public Newsletter() {
+    }
+
+    public Newsletter(String subject, String content) {
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Newsletter{" +
+                "subject='" + subject + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
