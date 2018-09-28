69317753c7467da7ecdab63b543b74e26a585e37
Balint Sipos <sipos.balint94@gmail.com>
2018-09-28 12:29:24 +0200

    Add: Thymeleaf: show subscriptions count in confirmation.html

--- src/main/java/org/marketing/newsletter/controller/ConfirmationController.java
+++ src/main/java/org/marketing/newsletter/controller/ConfirmationController.java
@@ -1,13 +1,15 @@
 package org.marketing.newsletter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ConfirmationController {
 
     @GetMapping("/thank-you")
-    public String getConfirmation(){
+    public String getConfirmation(Model model){
+        model.addAttribute("numberOfNewslettersSubscription",452);//FIXME: do not hardcode  this into the java code
         return "confirmation";
     }
 
