9bff1c4da9fdbb2c462ed611c6e05c2ee0bad0ff
Balint Sipos <sipos.balint94@gmail.com>
2018-09-28 12:49:29 +0200

    Add: Thymeleaf: show lateast newsletters in confirmation.html

--- src/main/java/org/marketing/newsletter/controller/ConfirmationController.java
+++ src/main/java/org/marketing/newsletter/controller/ConfirmationController.java
@@ -1,15 +1,25 @@
 package org.marketing.newsletter.controller;
 
+import org.marketing.newsletter.model.Newsletter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class ConfirmationController {
+
+    private final List<Newsletter> recentNewsletters= Arrays.asList(
+            new Newsletter("Extreme price-cut! 50% OFF!","..."),
+            new Newsletter("New store in W-London","...")
+    );
 
     @GetMapping("/thank-you")
     public String getConfirmation(Model model){
         model.addAttribute("numberOfNewslettersSubscription",452);//FIXME: do not hardcode  this into the java code
+        model.addAttribute("newsletterHighlights",recentNewsletters);
         return "confirmation";
     }
 
